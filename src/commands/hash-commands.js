4d4617ed502ebb9d8465247f0bdc2f6d1f4c93e9
korney <madkorney@gmail.com>
2022-06-12 16:40:19 +0300

    fix: fix errors in hash command

--- src/commands/hash-commands.js
+++ src/commands/hash-commands.js
@@ -1,40 +1,39 @@
 import path from 'node:path';
-import fs from 'fs/promises';
 import * as errors from '../common/error-handler.js';
-const { createHash } = await import('crypto');
-// import * as url from 'url';
-// const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import {createHash} from 'crypto';
+import * as fs from 'fs/promises';
 
 
-export const getHexHash = (pathToFile) => {
+export const getHexHash = async (pathToFile) => {
   let hash = createHash('sha256');
 
   try {
-    let fileToEncode = await fs.readFile(
+    let fileContent = await fs.readFile(
       pathToFile,
       {encoding: 'utf-8'}
     );
-    hash.update(fileToEncode);
+    hash.update(fileContent);
   }
   catch (err) {
-    throw err;
+    console.log(errors.OPERATION_ERROR_MESSAGE);
+      return false;
   }
 
   const hashHex = hash.digest('hex');
   return hashHex;
 }
 
-export const printHash = (pathToFile, currentPath) => {
-  const fullPathToFile = path.join(currentPath, filePath); // todo verify path
+
+
+export const printHash = async (pathToFile, currentPath) => {
+  const fullPathToFile = path.resolve(currentPath, pathToFile);
+
   try {
-    let hash = getHexHash(fullPathToFile);
+    let fileHash = await getHexHash(fullPathToFile);
+    if (fileHash) {console.log(`hash for ${fullPathToFile} is : ${fileHash}`);}
   }
   catch (err) {
     console.log(errors.OPERATION_ERROR_MESSAGE);
     return false;
   }
-  console.log(`hash is : ${hash}`);
 }
-
-
-
